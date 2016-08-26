216f7fd03cd344ac58492b3cdcf6f9f7ae1adf1d
Jaztsong <ztslx@hotmail.com>
2016-08-26 16:37:35 -0400

    modified:   parser.cpp

--- parser.cpp
+++ parser.cpp
@@ -324,19 +324,18 @@ void AP_stat::calc_Airtime()
 
                 double t_inc =0.0;
                 if(t_nav > 0){
-                        t_inc = t_bits/t_rate + 
-                                min(t_nav,t_next_delta);
+                        t_inc =  min(t_nav,t_next_delta);
                 }else{
                         t_inc = min(t_pre_delta,t_bits/t_rate);
                 }
                 mAirTime += t_inc;
 
-                /* D(setprecision(16)<<(*it)->getTime()<<"Airtime= "<< */
-                /*                 t_inc */
-                /*                 <<" NAV:"<<t_nav */
-                /*                 <<" NEXT: "<<t_next_delta */
-                /*                 <<" PRE: "<<t_pre_delta */
-                /*                 <<" bit/rate: "<<t_bits/t_rate); */
+                D(setprecision(16)<<(*it)->getTime()<<"Airtime= "<<
+                                t_inc
+                                <<" NAV:"<<t_nav
+                                <<" NEXT: "<<t_next_delta
+                                <<" PRE: "<<t_pre_delta
+                                <<" bit/rate: "<<t_bits/t_rate);
                     
         }
 }
